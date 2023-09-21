9cdc515816ff16d1e9be4b1b1860798a611f68c4
Ashan_264 <ashan@gatech.edu>
2023-09-21 15:51:19 -0400

    start screen

---
+++ app/src/main/java/com/example/s0/BlankScreen.java
@@ -0,0 +1,31 @@
+package com.example.s0;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class BlankScreen extends AppCompatActivity {
+
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.blank_screen);
+
+        }
+    }
+}
--- app/src/main/java/com/example/s0/MainActivity.java
+++ app/src/main/java/com/example/s0/MainActivity.java
@@ -2,55 +2,31 @@ package com.example.s0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MyStack stack;
-    private TextView topDisplay;
-    private EditText pushInput;
-    private EditText deleteInput;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startBtn = findViewById(R.id.startButton);
+        Button exitBtn =  findViewById(R.id.exitButton);
 
-        /*
-            This does not retain the stack object during configuration change
-            See: https://developer.android.com/guide/topics/resources/runtime-changes.html
-         */
-        stack = new MyStack();
-        topDisplay = (TextView) findViewById(R.id.topDisplay);
-        pushInput = (EditText) findViewById(R.id.pushInput);
-        deleteInput = (EditText) findViewById(R.id.deleteInput);
-    }
+        startBtn.setOnClickListener(v -> {
+            Intent game = new Intent(MainActivity.this, BlankScreen.class); // Ashan's Change
+            startActivity(game); // Ashan's Change
+        });
 
-    private void updateTopDisplay() {
-        if (stack.top() == null) {
-            topDisplay.setText("Error: null top");
-        } else {
-            topDisplay.setText(stack.top());
-        }
-    }
+        exitBtn.setOnClickListener(v -> {
+            finishAffinity();  // Close all activities and exit the app
+            System.exit(0);
+        });
 
-    public void onPush(View view) {
-        stack.push(pushInput.getText().toString());
-        pushInput.getText().clear();
-        updateTopDisplay();
-    }
-
-    public void onPop(View view) {
-        stack.pop();
-        updateTopDisplay();
-    }
-
-    public void onDelete(View view) {
-        stack.delete(Integer.parseInt(deleteInput.getText().toString()));
-        deleteInput.getText().clear();
-        updateTopDisplay();
     }
 }
