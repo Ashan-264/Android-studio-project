bcdc26d9ee6020fb37d992744bf0166a9d7103a1
Ashan_264 <ashan@gatech.edu>
2023-10-02 21:06:16 -0400

    folders for Model and view model

---
+++ app/src/main/java/com/example/s0/View-Model/GameScreen.java
@@ -0,0 +1,51 @@
+package com.example.s0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+public class GameScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_screen);
+
+        GameObject gameObject = GameObject.getGameObject();
+        Player player = gameObject.getPlayer();
+
+        // Display difficulty
+        TextView difficultyText = (TextView) findViewById(R.id.difficulty);
+        difficultyText.setText(gameObject.getDifficulty());
+
+        // Display player name
+        TextView playerNameText = (TextView) findViewById(R.id.gamePlayerName);
+        playerNameText.setText(player.getName());
+
+        // Display player Health
+        TextView playerHealthText = (TextView) findViewById(R.id.playerHealth);
+        playerHealthText.setText("Health: " + Integer.toString(player.getHealth()));
+
+        // Display player sprite
+        ImageView playerImage = (ImageView) findViewById(R.id.playerImage);
+        String spriteName = player.getSprite().getImageName();
+
+        if (spriteName.equals("Buzz")) {
+            playerImage.setImageResource(R.drawable.buzz2);
+        } else if (spriteName.equals("Wizard")) {
+            playerImage.setImageResource(R.drawable.purple_wizard);
+        } else if (spriteName.equals("Archer")) {
+            playerImage.setImageResource(R.drawable.green_archer);
+        }
+
+        Button gameButton =  findViewById(R.id.finishBtn);
+
+        gameButton.setOnClickListener(v -> {
+            Intent game = new Intent(this, EndScreen.class);
+            startActivity(game);
+        });
+    }
+}
