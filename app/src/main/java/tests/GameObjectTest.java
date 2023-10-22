de95d48efa24afccb9a649d26d5148dbbf2139cc
Rikhil Seshadri <rikhilseshadri@gmail.com>
2023-10-22 10:43:31 -0400

    sprint 2 tests

---
+++ app/src/main/java/tests/GameObjectTest.java
@@ -0,0 +1,53 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.*;
+
+import model.GameObject;
+import model.Player;
+
+public class GameObjectTest {
+    @Test
+    public void testingSingletonProperty() {
+        GameObject instance1 = GameObject.getGameObject();
+        GameObject instance2 = GameObject.getGameObject();
+
+        assertEquals(instance1, instance2);
+    }
+    @Test
+    public void testPlayer() {
+        Player newPlayer = new Player("Test name","Test Sprite",1);
+        String testDifficulty = "Hard";
+
+        GameObject gameObject = GameObject.getGameObject();
+        gameObject.configGame(newPlayer,testDifficulty);
+
+        assertEquals(newPlayer,  gameObject.getPlayer());
+
+
+    }
+
+    @Test
+    public void testDifficulty() {
+        Player newPlayer = new Player("Test name","Test Sprite",1);
+        String testDifficulty = "Hard";
+
+        GameObject gameObject = GameObject.getGameObject();
+        gameObject.configGame(newPlayer,testDifficulty);
+
+        assertEquals(testDifficulty, gameObject.getDifficulty());
+    }
+
+    @Test
+    public void testDefaultValues() {
+        GameObject gameObject = GameObject.getGameObject();
+        assertNull(gameObject.getPlayer());
+        assertNull(gameObject.getDifficulty());
+    }
+
+
+
+
+}
