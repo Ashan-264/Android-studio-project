7ea449a74b51717e6694f23186e586766023a2ad
Raghav R <RaghavR@lawn-143-215-93-93.lawn.gatech.edu>
2023-10-22 21:59:25 -0400

    8 tests

---
+++ app/src/main/java/tests/ScreenLimitTestUL.java
@@ -0,0 +1,31 @@
+package tests;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Player;
+
+public class ScreenLimitTestUL {
+
+    private Player player = Player.getPlayer();
+    @Before
+    public void initialization() {
+        player.setPlayer("George Burdell", "Burdell", 3);
+        player.setPlayerY(-100);
+        player.setPlayerX(10);
+    }
+
+    @Test
+    public void testUpperBound () {
+        player.moveUp(40);
+        assertEquals(-100, player.getPlayerY());
+    }
+    @Test
+    public void testLeftBound () {
+        player.moveLeft(40);
+        assertEquals(10, player.getPlayerX());
+    }
+
+}
