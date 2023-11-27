c861b38766217285346edec0ff7418455cf2f030
aagoran <atharvagorantiwar@lawn-143-215-103-213.lawn.gatech.edu>
2023-11-27 17:26:19 -0500

    removed a completed TODO

--- app/src/main/java/viewmodel/GameScreen2.java
+++ app/src/main/java/viewmodel/GameScreen2.java
@@ -128,6 +128,9 @@ public class GameScreen2 extends AppCompatActivity implements ScoreObserver, Hea
         playerHealthText = (TextView) findViewById(R.id.playerHealth);
         playerHealthText.setText("Health: " + Integer.toString(newHealth));
 
+        // TO DO
+        // Change this to Archer's class once he adds it
+        // This is when health hits 0, should go to EndScreenLose
         if (newHealth <= 0) {
             Intent game = new Intent(GameScreen2.this, EndScreen.class);
             startActivity(game);
